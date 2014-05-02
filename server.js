5b3b2ca1c351ffe496716696e6a666b007fe9000
sahilsk <sonu.k.meena@stackexpress.com>
2014-05-02 15:31:27 +0530

    updated transformer to use engine.io to make it work behind hipache(proxy)

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var server = http.createServer(app);
 
 
 // Primus server
-var primus = new Primus(server,{ transformer: 'websockets' });
+var primus = new Primus(server,{ transformer: 'engine.io' });
 // Use resource plugin
 primus
 .use('multiplex', 'primus-multiplex')
